bd91956b2d32fc8186c5b6413e3d0258b6d43632
Roberto Focosi <rfocosi@gmail.com>
2019-09-15 18:48:50 -0300

    Add linux builder script

---
+++ SRC/NETWORK/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+EXECNAME=$1
+
+if [ -z "$EXECNAME" ]; then
+  echo "Usage: $0 <executable_name>"
+  exit 1
+fi
+
+if [ ! -f "$EXECNAME.c" ]; then
+  echo "Not found $EXECNAME.c"
+  exit 1
+fi
+
+rm -f $EXECNAME.com
+
+sdcc --code-loc 0x180 --data-loc 0 -mz80 --disable-warning 196 --no-std-crt0 -L $SDCC_LIB/crt0_msxdos_advanced.rel $SDCC_LIB/printf.rel $SDCC_LIB/putchar_msxdos.rel asm.lib $EXECNAME.c
+
+if [ -f "$EXECNAME.ihx" ]; then
+  hex2bin -e com $EXECNAME.ihx
+fi
+
+rm -f $EXECNAME.rel $EXECNAME.asm
+rm -f *.ihx *.lk *.lst *.map *.noi *.sym
